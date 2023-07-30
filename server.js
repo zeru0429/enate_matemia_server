6fa24f8ff60f3b8dfed550086b1fe6320ba126da
zeru0429 <mihiretutigistu@gmail.com>
2023-07-30 11:10:04 +0300

    prepared local ip address

--- server.js
+++ server.js
@@ -4,17 +4,42 @@ import cors from 'cors';
 import mysql from 'mysql2'
 
 //constants
-const port = 8100
+const port = 8190;
+const ip = '192.168.1.11';
 
 // Set up a MySQL database connection pool
-const connection =mysql.createConnection ({
+const connection = mysql.createConnection({
+  ////Free MySQL Hosting
+  // host: 'sql7.freemysqlhosting.net',
+  // port: '3306',
+  // user: "sql7636185",
+  // password: "RPJ7CEKBk8",
+  // database: "sql7636185",
+
+ // //FreeDB Free Database Panel
+  // host: 'sql.freedb.tech',
+  // port: '3306',
+  // user: "freedb_enatmatemiya",
+  // password: "fRcMbfT@w6A@#X7",
+  // database: "freedb_enatematemiya",
+
+
+  ////localhost
   host: 'localhost',
   port: '3333',
   user: "root",
   password: "1234",
   database: "enate",
 
+})
+connection.connect((error) => {
+  if (error) console.error(error);
+  else {
+    console.log("db Connected sucessfully");
+  }
 });
+
+
 
 
 // Set up an Express app
@@ -130,18 +155,29 @@ app.delete('/deleteusers/:id', (req, res) => {
   });
 
 // delete product /deleteproducts/
-app.delete('/deleteproducts/:id', (req, res) => {
-  const userId = req.params.id;
+app.delete('/deleteproducts/:id:name', (req, res) => {
+  const productId = req.params.id;
+  const productName = req.params.name;
+  console.log(productId);
+  console.log(productName);
   // Delete the related records from the `orders` table first
-  const ordersQuery = 'DELETE FROM `products` WHERE `id` = ?';
-  connection.query(ordersQuery, [userId], (err, results, fields) => {
+  const ordersQuery = "DELETE FROM `orders` WHERE `product_name` = ?";
+  connection.query(ordersQuery, [productName], (err, results, fields) => {
     if (err) {
       console.log(err);
       res.status(404).send("Error occurred during deletion");
     } else {
       // If the deletion from the `orders` table was successful
-      res.send("Deletion successful");
-      console.log("Deletion successful");
+      const productsQuery = 'DELETE FROM `products` WHERE `id` = ?';
+      connection.query(productsQuery, [productId], (err, results, fields) => {
+        if (err) {
+          console.log(err);
+          res.status(404).send("Error occurred during deletion");
+        } else {
+          res.send("Deletion successful");
+          console.log("Deletion successful");
+        }
+      });
     }
   });
 });
@@ -218,6 +254,6 @@ app.get('/day', (req, res) => {
 
 
 // Start the server and listen on port 3000
-app.listen(port, () => {
-  console.log('Server listening on port 8100');
+app.listen(port,ip ,() => {
+  console.log(`Server listening on http://${ip}:${port}`);
 });
